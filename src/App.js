40bab3ddc6957c2c80f4ced7eaba82c3261fa0f7
jsb <tjdqls2021@gmail.com>
2023-11-09 16:18:00 +0900

    Spinner를 테이블 밖으로 뺌

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
-  useParams,
 } from "react-router-dom";
 import { BoardWrite } from "./page/BoardWrite";
 import { BoardList } from "./page/BoardList";
 import { HomeLayout } from "./layout/HomeLayout";
-import { Box, Spinner, Text } from "@chakra-ui/react";
-import axios from "axios";
-
-function BoardView() {
-  const [board, setBoard] = useState(null);
-  let { id } = useParams();
-
-  console.log(id);
-  useEffect(() => {
-    axios.get(`api/board/id${id}`).then((response) => setBoard(response.data));
-  }, []);
-  if (board === null) {
-    return <Spinner />;
-  }
-
-  return (
-    <Box>
-      <h1>글보기</h1>
-      <p>번호 : {board.id}</p>
-      <p>제목 : {board.title}</p>
-      <p>본문 : {board.content}</p>
-      <p>작성자 : {board.write}</p>
-      <p>작성일시 : {board.inserted}</p>
-    </Box>
-  );
-}
+import { BoardView } from "./page/BoardView";
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
