98f5c5d10886a26328d7bfeb06ef4010a083d829
jsb <tjdqls2021@gmail.com>
2023-11-15 15:28:54 +0900

    useRef로 댓글 id 임시 저장

--- src/component/CommentContainer.js
+++ src/component/CommentContainer.js
@@ -1,51 +1,194 @@
-import { Box, Button, Input, Spinner, Textarea } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Heading,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Stack,
+  StackDivider,
+  Text,
+  Textarea,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
 import axios from "axios";
-import { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import loginProvider, { LoginContext } from "./LoginProvider";
+import { DeleteIcon } from "@chakra-ui/icons";
 
-function CommentForm({ boardId }) {
+function CommentForm({ boardId, isSubmitting, onSubmit }) {
   const [comment, setComment] = useState("");
 
   function handleSubmit() {
-    axios.post("/api/comment/add", {
-      boardId,
-      comment,
-    });
+    onSubmit({ boardId, comment });
   }
 
   return (
     <Box>
       <Textarea value={comment} onChange={(e) => setComment(e.target.value)} />
-      <Button onClick={handleSubmit}>쓰기</Button>
+      <Button isDisabled={isSubmitting} onClick={handleSubmit}>
+        쓰기
+      </Button>
     </Box>
   );
 }
 
-function CommentList({ boardId }) {
-  const [commentList, setCommentList] = useState("null");
-  useEffect(() => {
-    const params = new URLSearchParams();
-    params.set("id",boardId)
-    axios
-      .get(`api/comment/list?${params}`)
-      .then((response) => setCommentList(response.data));
-  }, []);
-
-  if (commentList === null) {
-    return <Spinner />;
-  }
+function CommentList({ commentList, onDeleteModalOpen, isSubmitting }) {
+  const { hasAccess, isAdmin } = useContext(LoginContext);
 
   return (
-    <Box>
-      {commentList.map}
-   </Box>
+    <Card>
+      <CardHeader>
+        <Heading size="md">댓글 리스트</Heading>
+      </CardHeader>
+      <CardBody>
+        <Stack divider={<StackDivider />} spacing="4">
+          {commentList.map((comment) => (
+            <Box key={comment.id}>
+              <Flex justifyContent="space-between">
+                <Heading size="xs">{comment.memberId}</Heading>
+                <Text fontSize="xs">{comment.inserted}</Text>
+              </Flex>
+              <Flex justifyContent="space-between" alignItems="center">
+                <Text sx={{ whiteSpace: "pre-wrap" }} pt="2" fontSize="sm">
+                  {comment.comment}
+                </Text>
+                {(hasAccess(comment.memberId) || isAdmin()) && (
+                  <Button
+                    colorScheme="red"
+                    size="xs"
+                    isDisabled={isSubmitting}
+                    onClick={() => onDeleteModalOpen(comment.id)}
+                  >
+                    <DeleteIcon />
+                  </Button>
+                )}
+              </Flex>
+            </Box>
+          ))}
+        </Stack>
+      </CardBody>
+    </Card>
   );
 }
 
 export function CommentContainer({ boardId }) {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  // const [id, setId] = useState(0);
+  // useRef : 컴포넌트에서 임시로 값을 저장하는 용도로 사용
+  //
+  const commentIdRef = useRef(0);
+  const [commentList, setCommentList] = useState([]);
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const toast = useToast();
+  const { isAuthenticated } = useContext(LoginContext);
+  function handleSubmit(comment) {
+    setIsSubmitting(true);
+    axios
+      .post("/api/comment/add", comment)
+      .then(() => {
+        toast({
+          description: "댓글 작성 등록",
+          status: "success",
+        });
+      })
+      .catch((error) => {
+        if (error.response.status === 400) {
+          toast({
+            description: "잘못된 요청입니다.",
+            status: "warning",
+          });
+        } else if (error.response.status === 401) {
+          toast({
+            description: "로그인후 이용해주세요..",
+            status: "warning",
+          });
+        } else {
+          toast({
+            description: "서버에서 문제가 발생했습니다",
+            status: "error",
+          });
+        }
+      })
+      .finally(() => setIsSubmitting(false));
+  }
+  function handleDelete() {
+    setIsSubmitting(true);
+    // console.log(id + "번 댓글 삭제");
+    //  TODO : 모달,then catch finally
+    axios
+      .delete(`/api/comment/${commentIdRef.current}`)
+      .then(() => console.log("성공"))
+      .catch(() => console.log("실패"))
+      .finally(() => {
+        setIsSubmitting(false);
+        onClose();
+      });
+  }
+
+  useEffect(() => {
+    if (!isSubmitting) {
+      const params = new URLSearchParams();
+      params.set("id", boardId);
+      axios
+        .get(`/api/comment/list?${params}`)
+        .then((response) => setCommentList(response.data));
+    }
+  }, [isSubmitting]);
+
+  function handleDeleteModalOpen(id) {
+    // id를 어딘가 저장
+    // setId(id);
+    commentIdRef.current = id;
+    // 모달 열기
+    onOpen();
+  }
+
   return (
     <Box>
-      <CommentForm boardId={boardId} />
-      <CommentList boardId={boardId} />
+      {isAuthenticated() && (
+        <CommentForm
+          boardId={boardId}
+          isSubmitting={isSubmitting}
+          onSubmit={handleSubmit}
+        />
+      )}
+
+      <CommentList
+        boardId={boardId}
+        isSubmitting={isSubmitting}
+        commentList={commentList}
+        onDeleteModalOpen={handleDeleteModalOpen}
+      />
+
+      {/* 댓글 삭제 모달 */}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>삭제 확인</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>삭제 하시겠습니까 ?</ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>닫기</Button>
+            <Button
+              isDisabled={isSubmitting}
+              onClick={handleDelete}
+              colorScheme="red"
+            >
+              삭제하기
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
