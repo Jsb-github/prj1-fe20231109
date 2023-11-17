8dc4eb13bac2a71db8788a8e1aca7fdf7ecaee31
jsb <tjdqls2021@gmail.com>
2023-11-17 15:15:39 +0900

    페이지 버튼 만들기(임의)

--- src/page/board/BoardList.js
+++ src/page/board/BoardList.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import {
   Badge,
   Box,
+  Button,
+  Flex,
   Spinner,
   Table,
   Tbody,
@@ -11,7 +13,7 @@ import {
   Tr,
 } from "@chakra-ui/react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { ChatIcon } from "@chakra-ui/icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -21,14 +23,13 @@ import {
 
 export function BoardList() {
   const [boardList, setBoardList] = useState(null);
-
+  const [params] = useSearchParams();
   const navigate = useNavigate();
-
   useEffect(() => {
     axios
-      .get("/api/board/list")
+      .get("/api/board/list?" + params)
       .then((response) => setBoardList(response.data));
-  }, []);
+  }, [params]);
 
   if (boardList === null) {
     return <Spinner />;
@@ -80,6 +81,20 @@ export function BoardList() {
             ))}
           </Tbody>
         </Table>
+        <Flex>
+          <Box>
+            <Button onClick={() => navigate("/?p=1")}>1</Button>
+            <Button onClick={() => navigate("/?p=2")}>2</Button>
+            <Button onClick={() => navigate("/?p=3")}>3</Button>
+            <Button onClick={() => navigate("/?p=4")}>4</Button>
+            <Button onClick={() => navigate("/?p=5")}>5</Button>
+            <Button onClick={() => navigate("/?p=6")}>6</Button>
+            <Button onClick={() => navigate("/?p=7")}>7</Button>
+            <Button onClick={() => navigate("/?p=8")}>8</Button>
+            <Button onClick={() => navigate("/?p=9")}>9</Button>
+            <Button onClick={() => navigate("/?p=10")}>10</Button>
+          </Box>
+        </Flex>
       </Box>
     </Box>
   );
